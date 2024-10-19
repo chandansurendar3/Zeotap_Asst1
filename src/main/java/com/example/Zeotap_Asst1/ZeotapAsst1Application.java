01dcc934c0bc15a6484ce1571fd7a2573e2ad676
chandansurendar3 <chandansurendar3@gmail.com>
2024-10-19 16:43:38 +0530

    initial commit

--- src/main/java/com/example/Zeotap_Asst1/ZeotapAsst1Application.java
+++ src/main/java/com/example/Zeotap_Asst1/ZeotapAsst1Application.java
@@ -8,7 +8,7 @@ public class ZeotapAsst1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZeotapAsst1Application.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World1");
 	}
 
 }
